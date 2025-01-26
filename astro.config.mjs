e0e2b8a0429d133e1ff651243cd6a2f842155d6c
mdandaur <mdandaur@DESKTOP-FBLJ05G>
2025-01-26 12:46:32 -0300

    build: Try 1 deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mdandaur.github.io',
+  base: 'jiko-no-kansei-app',
   integrations: [sitemap()],
   experimental: {
     svg: true,
