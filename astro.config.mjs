43145350da3d11c2851e647cf09cd9f31aff773a
mdandaur <mdandaur@DESKTOP-FBLJ05G>
2025-03-09 19:45:10 -0300

    fix(AboutUs): modifies the text inside about us to the final text

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
 
   experimental: {
     svg: true,
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'; 
+
+const Carousel = ({ images }) => {
+    [index, setIndex] = useState(0);
+    
+    const handleNext = () => {
+        
+
+}
+
+export default Carousel;
